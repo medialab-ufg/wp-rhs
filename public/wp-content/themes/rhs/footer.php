a05eafc9e3f4b1cc2248ba1caf9cff4ed8f67f57
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-04-11 12:00:55 -0300

    Envio dos arquivos alterados.

---
+++ public/wp-content/themes/rhs/footer.php
@@ -0,0 +1,8 @@
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- JavaScript Latest compiled and minified JavaScript -->
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
