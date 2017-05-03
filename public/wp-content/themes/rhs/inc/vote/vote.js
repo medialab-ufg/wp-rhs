d99bd7162f854af00e3efeeb1b2fd477712293ac
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-05-03 10:18:35 -0300

    Adicionado icone de carregamento ao clicar no botão votar.

--- public/wp-content/themes/rhs/inc/vote/vote.js
+++ public/wp-content/themes/rhs/inc/vote/vote.js
@@ -2,7 +2,7 @@ jQuery( function( $ ) {
 
     $('.js-vote-button').click(function() {
         var post_id = $(this).data('post_id');
-        $('#votebox-'+post_id).html('...').load(
+        $('#votebox-'+post_id).html('<i class="fa fa-spinner fa-pulse fa-fw"></i>').load(
             vote.ajaxurl, 
             {
                 action: 'rhs_vote', 
