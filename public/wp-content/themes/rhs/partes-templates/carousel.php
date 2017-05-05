c0bcbedbaf55640387a9573ca80ed3827de5fc4c
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-05-05 11:33:57 -0300

    Carousel adicionado para Descktop. #14

--- public/wp-content/themes/rhs/partes-templates/carousel.php
+++ public/wp-content/themes/rhs/partes-templates/carousel.php
@@ -1,16 +1,19 @@
-<div id="myCarousel" class="carousel slide">
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-		<li data-target="#myCarousel" data-slide-to="2"></li>
-		<li data-target="#myCarousel" data-slide-to="3"></li>
-		<li data-target="#myCarousel" data-slide-to="4"></li>
-	</ol>
-	<div class="carousel-inner" style="min-height: 320px; background-color: #fff">
-		<?php if(have_posts()) :
+<div id="carousel-example-generic" class="carousel slide carousel-fade" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+  </ol>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox" style="min-height: 320px; background-color: #fff">
+  	<?php if(have_posts()) :
+  				$first = true;
 				while (have_posts()): 
-					the_post(); ?>
-		<div class="item">
+					the_post(); ?>	
+	    <div class="item <?php if($first) echo 'active'; ?>">
 			<div class="row">
 				<div class="col-xs-6">
 					<?php if( has_post_thumbnail() ) :  ?>
@@ -18,6 +21,7 @@
 							<img src="<?php get_the_post_thumbnail_url() ?>" alt="">
 						</a>
 					<?php endif; ?>
+
 				</div>
 				<div class="col-xs-6">
 					<div class="carousel-caption">
@@ -28,8 +32,19 @@
 					</div>
 				</div>
 			</div>
-		</div>
-		<?php endwhile; endif; ?>
-	</div>
+	    </div>
+	    <?php $first = false; ?>
+	    <?php endwhile; endif; ?>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
 <?php wp_reset_query(); ?>
