33e7ff128eed3ebf40c59fed9ffcb7751e7634e6
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-05-08 15:02:16 -0300

    Retirado o FontAwesome de ser importado para ser adicionado ao repositório; Continuação da pagina de login.

--- public/wp-content/themes/rhs/page-login.php
+++ public/wp-content/themes/rhs/page-login.php
@@ -3,17 +3,54 @@
 <div class="row">
 	<!-- Container -->
 	<div class="col-xs-12 col-md-9">
-		<div class="row">
-			<div class="col-xs-12"></div>
-		</div>
-		<div class="row">
-			<?php
-				$args = array(
-				    'redirect' => home_url(),
-				   );
-			?>
-			<?php wp_login_form($args); ?>
-		</div>
+		<?php
+			$args = array(
+			    'redirect' => home_url(),
+			   );
+		?>
+		<?php 
+	    if(is_user_logged_in()) : ?>
+    		<section class="loginpanel">
+				<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
+					<div class="panel panel-info" >
+						<div class="panel-heading">
+							<div class="panel-title">Acessar sua Conta</div>
+							<div class="panel-sub"><a href="#">Esqueceu a Senha?</a></div>
+						</div>     
+
+						<div class="panel-body" >
+							<form class="form-horizontal" role="form">
+								<div class="form-group float-label-control">
+									<label for="user_login">Email</label>
+									<input type="text" tabindex="1" name="log" id="user_login" class="form-control" value="" size="20" required>                               
+								</div>
+								<div class="form-group float-label-control">
+									<label for="user_pass">Senha</label>
+									<input type="password" tabindex="2" name="pwd" id="user_pass" class="form-control" value="" size="20">
+								</div>
+								<div class="form-group">
+									<div class="checkbox">
+										<label>
+											<input id="login-remember" type="checkbox" name="remember" value="1"> Salvar dados de Acesso?
+										</label>
+									</div>
+								</div>
+								<div class="form-group">
+									<div class="col-md-12">
+										<div class="panel-other" >
+											Você não tem uma conta?
+											<a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
+												Crie uma aqui!
+											</a>
+										</div>
+									</div>
+								</div>    
+							</form>    
+						</div>                     
+					</div>  
+				</div>
+    		</section>
+		<?php endif; ?>
 	</div>
 
 	<!-- Sidebar -->
