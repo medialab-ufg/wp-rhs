b8ecba41f23bed36b6dada62ba8752b6d2f545cd
Leo Germani <leogermani@hacklab.com.br>
2017-05-08 16:03:52 -0300

    importa avatar com url original (refs #8)

--- migration-scripts/sql/users-meta-avatar.sql
+++ migration-scripts/sql/users-meta-avatar.sql
@@ -12,7 +12,7 @@ SELECT DISTINCT
 
     u.uid as `user_id`,
     'rhs_avatar' as `meta_key`,
-    CONCAT('wp-content/uploads/2017/01/',fm.filename) as `meta_value`
+    fm.uri as `meta_value`
     FROM {{drupaldb}}.users u
     INNER JOIN {{drupaldb}}.file_managed fm ON u.picture = fm.fid
 
