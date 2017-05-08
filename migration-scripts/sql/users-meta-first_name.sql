27df71190ee09bbb86110817ad824920ec25bde7
Leo Germani <leogermani@hacklab.com.br>
2017-05-08 15:33:20 -0300

    remove INNER JOIN desnecessário e arruma identação (refs #8)

--- migration-scripts/sql/users-meta-first_name.sql
+++ migration-scripts/sql/users-meta-first_name.sql
@@ -7,10 +7,10 @@ INSERT IGNORE INTO {{usermeta}}
 )
 
 SELECT DISTINCT
+
     u.uid as `user_id`,
     'first_name' as `meta_key`,
     SUBSTRING_INDEX(u.name,' ',1) as `meta_value`
-      FROM {{drupaldb}}.users u
-        INNER JOIN {{drupaldb}}.url_alias a
-        ON a.source = CONCAT('user/', u.uid)
-          WHERE u.uid > 1;
+    FROM {{drupaldb}}.users u
+
+WHERE u.uid > 1;
