12d6c6455e1d528fe08061aac24246298f27cc61
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-05-10 12:34:10 -0300

    Adicionado partes da Validação a página de cadastro e Organizado o Layout! #2

--- public/wp-content/themes/rhs/assets/js/valida-form-registro.js
+++ public/wp-content/themes/rhs/assets/js/valida-form-registro.js
@@ -3,21 +3,9 @@ jQuery( function( $ ) {
     $(function () {
                 
                 $("#form-cadastro").validate({
-                    highlight: function(element) {
-                        $(element).parent().addClass('has-error');
-                    },
-                    unhighlight: function(element) {
-                        $(element).parent().removeClass('has-error');
-                    },
-                    success: function(element) {
-                        $(element).parent().addClass('has-success');
-                    },
-                    debug: true,
-                    focusCleanup: true,
-                    errorLabelContainer: "input .label",
-                    errorElement: "div",
                     rules: {
                         name: {
+                            required: true,
                             minlength: 9,
                             maxlength: 50
                         },
@@ -28,15 +16,52 @@ jQuery( function( $ ) {
                     },
                     messages: {
                         name: {
-                            required: "",
+                            required: "O nome é necessário!",
                             minlength: "Insira seu nome completo!"
                         },
-                        mail: {
-                            required: "",
-                            email: "Preencha corretamento seu email!"
+                        mail: "Preencha corretamento seu email!"
+                    },
+                    errorContainer: ".block-email", 
+                    errorLabelContainer: ".block-email", 
+                    errorElement: "dt",
+                    errorPlacement: function ( error, element ) {
+                        // Add the `help-block` class to the error element
+                        error.addClass( "help-block" );
+
+                        // Add `has-feedback` class to the parent div.form-group
+                        // in order to add icons to inputs
+                        element.parents( ".col-sm-12" ).addClass( "has-feedback" );
+
+                        if ( element.prop( "type" ) === "checkbox" ) {
+                            error.insertIn( element.parent( "label" ) );
+                        } else {
+                            error.appendTo( element );
                         }
+
+                        // Add the span element, if doesn't exists, and apply the icon classes to it.
+                        if ( !element.next( "span" )[ 0 ] ) {
+                            $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+                        }
+                    },
+                    success: function ( label, element ) {
+                        // Add the span element, if doesn't exists, and apply the icon classes to it.
+                        if ( !$( element ).next( "span" )[ 0 ] ) {
+                            $( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
+                        }
+                        
+                    },
+                    highlight: function ( element, errorClass, validClass ) {
+                        $( element ).parents( ".col-sm-12" ).addClass( "has-error" ).removeClass( "has-success" );
+                        $( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
+                    },
+                    unhighlight: function ( element, errorClass, validClass ) {
+                        $( element ).parents( ".col-sm-12" ).addClass( "has-success" ).removeClass( "has-error" );
+                        $( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
                     }
                 });
             });
 
 });
+
+
+       
--- public/wp-content/themes/rhs/page-cadastro.php
+++ public/wp-content/themes/rhs/page-cadastro.php
@@ -3,37 +3,69 @@
 <div class="row">
 	<!-- Container -->
 	<div class="col-xs-12 col-md-12 login">
-    		<header class="userpage">
+    	<header class="userpage">
+
+			<form class="form-horizontal" role="form" id="form-cadastro">
 				<div class="col-md-6 col-md-offset-1 col-sm-8">                    
 					<div class="panel panel-info" >
 						<div class="panel-heading">
-							<div class="panel-title">Criar Conta</div>
+							<div class="panel-title">Dados de Login</div>
 						</div>     
 
 						<div class="panel-body" >
-							<form class="form-horizontal" role="form" id="form-cadastro">
 								<div class="form-group float-label-control">
-									<label for="edit-mail">Email</label>
-									<input class="form-control form-text" type="text" id="edit-mail" name="mail" size="60" maxlength="254" required>                     
+									<label for="edit-mail" class="col-sm-1 control-label">Email</label>
+									<div class="col-sm-12">
+										<input class="form-control form-text" type="email" id="edit-mail" name="mail" maxlength="254" required>
+									</div>
 								</div>
 								<div class="form-group float-label-control">
-									<label for="edit-name">Nome Completo</label>
+									<label for="pass" class="col-sm-1 control-label">Senha</label>
+									<div class="col-sm-12">
+										<input class="form-control form-text" type="password" id="pass" name="pass" maxlength="50" required> 
+										<div class="checkbox">
+											<label>
+												<input type="checkbox"> Exibir Senha
+											</label>
+										</div>
+									</div>
+								</div>
+						</div>                  
+					</div>  
+					<div class="panel panel-info">
+						<div class="panel-heading">
+							<div class="panel-title">Dados Pessoais</div>
+						</div>
+						<div class="panel-body">
+							<div class="form-group float-label-control">
+								<label for="edit-name" class="col-sm-4 control-label">Nome Completo</label>
+								<div class="col-sm-12">
 									<input class="form-control form-text" type="text" id="edit-name" name="name" size="60" maxlength="60" required>
 								</div>
-								<div class="form-group">
-									<label for="sobre-mim">Sobre mim <em>(opcional)</em></label>
+							</div>
+							<div class="form-group float-label-control">
+								<label for="sobre-mim" class="col-sm-5 control-label">Sobre mim</label>
+								<div class="col-sm-12">
 									<textarea class="form-control form-textarea" id="sobre-mim" name="sobre-mim" cols="60" rows="5"></textarea>
 								</div>
-                                <div class="form-group">
-                                    <div class="col-sm-12 controls">
-                                      <a id="btn-login" href="#" class="btn btn-danger">CRIAR NOVA CONTA  </a>
-                                    </div>
-                                </div>   
-							</form>    
-						</div>                     
-					</div>  
+							</div> 
+						</div>
+					</div>
+                    <div class="form-group">
+                        <div class="col-sm-12 controls">
+                          <a id="btn-login" href="#" class="btn btn-danger">CRIAR NOVA CONTA  </a>
+                        </div>
+                    </div>
 				</div>
-    		</header>
+			</form>
+				<div class="col-md-4">
+					<div class="panel block-info" style="background: #f2f3f5;">    
+						<div class="panel-body">
+							<span class="block-email"></span>
+						</div> 
+					</div>
+				</div>
+    	</header>
 	</div>
 </div>
 
