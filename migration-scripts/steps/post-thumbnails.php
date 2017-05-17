838cab991fdd2f64e1962c30875b29dcc5fcebb2
Leo Germani <leogermani@hacklab.com.br>
2017-05-17 11:22:53 -0300

    script de importacao de thumbnails limpa melhor o que faz

--- migration-scripts/steps/post-thumbnails.php
+++ migration-scripts/steps/post-thumbnails.php
@@ -1,5 +1,7 @@
 <?php
 
+$logFilesCreatedFilename = WP_CONTENT_DIR . "/uploads/post-thumbnails-imported.json";
+
 $this->log('Limpando base de dados de thumbnails');
 $tids = $wpdb->get_col("SELECT meta_value FROM $wpdb->postmeta WHERE meta_key = '_thumbnail_id'");
 
@@ -10,6 +12,16 @@ if (sizeof($tids) > 0) {
     $wpdb->query("DELETE FROM $wpdb->postmeta WHERE meta_key = '_thumbnail_id' ");
 }
 
+$this->log('Limpando arquivos gerados se o script já foi rodado antes');
+$files = @file_get_contents($logFilesCreatedFilename);
+if ($files) {
+    $files = json_decode($files);
+    if (is_array($files)) {
+        foreach ($files as $f)
+            @unlink($f);
+    }
+}
+@unlink($logFilesCreatedFilename);
 
 $this->log('Setando tamanho dos thumbnails para 450px');
 update_option( 'thumbnail_size_w', 450 );
@@ -17,7 +29,9 @@ update_option( 'thumbnail_size_h', 450 );
 update_option( 'thumbnail_crop', 0 );
 
 $this->log('Pegando imagens destacadas no drupal');
-$images = $wpdb->get_results('select distinct * from '. RHS_DRUPALDB . '.field_data_field_home_image JOIN '. RHS_DRUPALDB . '.file_managed f ON f.fid = field_home_image_fid JOIN '. RHS_DRUPALDB . '.node on nid = entity_id');
+$images = $wpdb->get_results('select distinct * from '. RHS_DRUPALDB . '.field_data_field_home_image 
+                JOIN '. RHS_DRUPALDB . '.file_managed f ON f.fid = field_home_image_fid 
+                JOIN '. RHS_DRUPALDB . '.node on nid = entity_id LIMIT 5');
 
 require_once ABSPATH . 'wp-admin/includes/import.php';
 require_once ABSPATH . 'wp-admin/includes/image.php';  
@@ -37,9 +51,11 @@ function get_root_path() {
 $success = 0;
 $error = 0;
 
+// Save all the files created so we can clean up the next time we run the script
+$filesCreated = [];
+
 foreach ($images as $image) {
     
-
     // get placeholder file in the upload dir with a unique, sanitized filename
     $upload = wp_upload_bits( $image->filename, null, '', date('Y/m', $image->created) ); // passando a data de criação do post
     if ( $upload['error'] ) {
@@ -47,6 +63,8 @@ foreach ($images as $image) {
         $error++;
         continue;
     }
+    
+    $filesCreated[] = $upload['file'];
     
     // move the file
     $sourcefile = str_replace('public://', get_root_path() . '/sites/default/files/', $image->uri);
@@ -72,9 +90,20 @@ foreach ($images as $image) {
         
         
         $post_id = wp_insert_attachment( $newatt, $upload['file'] );
-        wp_update_attachment_metadata( $post_id, wp_generate_attachment_metadata( $post_id, $upload['file'] ) );
-        
+        $att_metadata = wp_generate_attachment_metadata( $post_id, $upload['file'] );
+        wp_update_attachment_metadata( $post_id, $att_metadata );
         add_post_meta($image->nid, '_thumbnail_id', $post_id);
+
+        if (is_array($att_metadata['sizes'])) {
+        
+            foreach ($att_metadata['sizes'] as $size => $sizefile) {
+            
+                if (is_array($sizefile) & isset($sizefile['file']))
+                    $filesCreated[] = WP_CONTENT_DIR . '/uploads/' . date('Y/m/', $image->created) . $sizefile['file'];
+            
+            }
+        
+        }
         
         $success ++;
         echo '.';
@@ -87,6 +116,16 @@ foreach ($images as $image) {
     
     } 
     
+}
+
+if ($success > 0) {
+    echo "\n";
+
+    $filesCreatedJson = fopen($logFilesCreatedFilename, "w");
+    fwrite($filesCreatedJson, json_encode($filesCreated));
+    fclose($filesCreatedJson);
+    $this->log('Registro das imagens criadas salvo em ' . $logFilesCreatedFilename);
+
 }
 
 $this->log('Total de erros: ' . $error);
