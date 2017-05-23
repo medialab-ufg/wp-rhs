f69f5d9ec0d73151ffe70d53908896f456585159
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-05-23 14:22:40 -0300

    Tela minhas postagens. (ref #33)

--- public/wp-content/themes/rhs/minhas-postagens.php
+++ public/wp-content/themes/rhs/minhas-postagens.php
@@ -26,39 +26,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php
-                                    global $current_user;
-                                    wp_get_current_user();
-                                    $author_query = array('posts_per_page' => '-1','author' => $current_user->ID);
-                                    $author_posts = new WP_Query($author_query);
-                                    $RHSVote = new RHSVote();
-                                    while($author_posts->have_posts()) : $author_posts->the_post();
-                                    ?>
-                                        <tr>
-                                            <td><?php the_ID(); ?></td>
-                                            <td><?php the_time('D, d/m/Y - H:i'); ?></td>
-                                            <td></td>
-                                            <td>
-                                                <?php
-                                                    if ( comments_open() ) :
-                                                      comments_popup_link( '0', '1 ', '%', '', '<i class="fa fa-ban" aria-hidden="true"></i>');
-                                                    endif;
-                                                ?>
-                                            </td>
-                                            <td>
-                                            <?php
-                                                $votos = $RHSVote->get_total_votes(get_the_ID());
-                                                if($votos <= 0){
-                                                    echo '0';
-                                                }else {
-                                                    echo $votos;
-                                                }
-                                            ?>
-                                            </td>
-                                        </tr>   
-                                    <?php           
-                                    endwhile;
-                                    ?>
+                                <?php RHSPost::minhasPostagens(); ?>
                             </tbody>
                         </table>
                     </div>
