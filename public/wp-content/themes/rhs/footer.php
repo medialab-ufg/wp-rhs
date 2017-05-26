f19a8101cbbb4c5300056b59f97ec06d4a9f3c8d
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-05-26 13:50:07 -0300

    Fazer rodapé ter largura 100%. (ref. 45)

--- public/wp-content/themes/rhs/footer.php
+++ public/wp-content/themes/rhs/footer.php
@@ -4,28 +4,26 @@
 		</section><!-- /.section -->
 
 		<footer class="footer">
-			<div class="container">
-				<section class="footerDescricao">
-					<p><?php bloginfo( 'description' ); ?></p>
-				</section>
-				<section class="footerMenu">
-					<nav class="navbar navbar-default">
-						<?php
-		                    /*
-	                        * menuFundo vem de um register feito nas functions onde o mesmo entra em contato com o menu do 
-	                        * Wordpress.
-	                        */
-			                menuRodape();
-			            ?>
-					</nav>
-				</section>
+			<section class="footerDescricao">
+				<p><?php bloginfo( 'description' ); ?></p>
+			</section>
+			<section class="footerMenu">
+				<nav class="navbar navbar-default">
+					<?php
+	                    /*
+                        * menuFundo vem de um register feito nas functions onde o mesmo entra em contato com o menu do 
+                        * Wordpress.
+                        */
+		                menuRodape();
+		            ?>
+				</nav>
+			</section>
 
-				<section class="corporation">
-					<div class="bordar"></div>
-					<img alt="Licença Creative Commons" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icreativecommons.png" style="border-width:0">
-					<a href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.pt" rel="license" target="_blank"> Conteúdos do site sob Licença&nbsp;Creative Commons - Atribuição-Não Comercial-Sem Derivados 3.0 Não Adaptada.</a>
-				</section>
-			</div>
+			<section class="corporation">
+				<div class="bordar"></div>
+				<img alt="Licença Creative Commons" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icreativecommons.png" style="border-width:0">
+				<a href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.pt" rel="license" target="_blank"> Conteúdos do site sob Licença&nbsp;Creative Commons - Atribuição-Não Comercial-Sem Derivados 3.0 Não Adaptada.</a>
+			</section>
 		</footer>
 		<?php wp_footer(); ?>    
 	</body>
