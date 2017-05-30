5674ed9cf491d45b83d853e3933f1855d1e415cb
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-05-30 14:16:19 -0300

    Fix o footer da pagina author para quando não houver post.

--- public/wp-content/themes/rhs/partes-templates/none.php
+++ public/wp-content/themes/rhs/partes-templates/none.php
@@ -1,4 +1,3 @@
-<div class="row">
 	<div class="col-xs-12 col-md-12">
 		<header class="page-header">
 			<h1 class="titulo-page"><?php _e( 'Não há posts', 'rhs' ); ?>
@@ -8,4 +7,3 @@
 		
 			<p style="background: #FFF; padding: 15px; font-weight: bold"><?php _e( 'Parece que não podemos encontrar o que você está procurando. Talvez a pesquisa possa ajudar.', 'rhs' ); ?></p>
 	</div>
-</div>
