9aeede569e9712d880de72bab07e26f6d775eb71
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-05-31 13:09:56 -0300

    Criar template para páginas. (ref. #49)

---
+++ public/wp-content/themes/rhs/page.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+
+<div class="row">
+	<!-- Container -->
+	<div class="col-xs-12 col-md-9">
+		<div class="row">
+			<!-- Button Publicar e Ver Fila de Votação -->
+			<?php get_template_part('partes-templates/buttons-top' ); ?>
+		</div>
+		<div class="row">
+			<?php 
+				while (have_posts()): the_post();
+			?>
+			<article id="post-<?php the_ID(); ?>">
+				<div class="col-xs-12">
+					<div class="panel panel-default padding-bottom">
+						<div class="panel-heading" style="border-bottom: 1px solid rgba(119, 119, 119, 0.13);">
+							<div class="row post-titulo">
+								<?php $userOBJ = new RHSUser(get_the_author_meta( 'ID' )); ?>
+								<div class="col-xs-9 col-sm-11 col-md-10">
+									<?php the_title( '<h3>', '</h3>' ); ?>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+						<div class="panel-body content">
+							<?php the_content(); ?>
+						</div><!-- .paine-body -->
+						<div class="col-xs-12">
+							<div class="tags-content">	
+							<?php if(has_category()) : ?>
+								<span class="tags-list">
+									<i class="fa fa-archive" aria-hidden="true" title="Categoria"></i>
+									<?php the_category(' '); ?>
+								</span>
+							<?php endif; ?>		
+							<?php if(has_tag()) : ?>
+								<span class="tags-list">
+									<i class="fa fa-tags" aria-hidden="true"  title="Tags"></i>
+									<?php the_tags(' '); ?>
+								</span>
+							<?php endif; ?>
+							</div>
+						</div>
+					</div><!-- .panel .panel-default -->
+				</div>
+			</article><!-- #post-## -->
+			<?php endwhile;	?>
+		</div>
+	</div>
+
+	<!-- Sidebar -->
+	<div class="col-xs-12 col-md-3"><?php get_sidebar(); ?></div>
+</div>
+
+<?php get_footer();
