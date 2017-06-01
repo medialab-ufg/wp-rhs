dc0d139b5eaf096797fdee7bdd06d1586da1e66d
William Avelar <avelardesigner@gmail.com>
2017-06-01 15:16:14 -0300

    Replace dos avatares com public, para o caminho correto

--- migration-scripts/sql/users-meta-avatar.sql
+++ migration-scripts/sql/users-meta-avatar.sql
@@ -12,7 +12,7 @@ SELECT DISTINCT
 
     u.uid as `user_id`,
     'rhs_avatar' as `meta_key`,
-    fm.uri as `meta_value`
+    IF(fm.uri LIKE 'public://%%',REPLACE(fm.uri, 'public://', 'sites/default/files/styles/profile-photo/public/'),fm.uri)  as `meta_value`
     FROM {{drupaldb}}.users u
     INNER JOIN {{drupaldb}}.file_managed fm ON u.picture = fm.fid
 
