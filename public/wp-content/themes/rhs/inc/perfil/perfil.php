614aae9ae608c2730d25f3c1ad8370bc88f114ec
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-06-01 13:35:55 -0300

    ao se cadastrar ou login, ir pra home! (ref #52)

--- public/wp-content/themes/rhs/inc/perfil/perfil.php
+++ public/wp-content/themes/rhs/inc/perfil/perfil.php
@@ -190,7 +190,7 @@ class RHSPerfil extends RHSMenssage {
     }
 
     static function url(){
-        return esc_url(home_url());
+        return esc_url(home_url()); //mudado de home_url(RHSRewriteRules::PROFILE_URL)
     }
 }
 
