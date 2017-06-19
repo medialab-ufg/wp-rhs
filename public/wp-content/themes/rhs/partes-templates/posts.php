057486c54fbd2a52ec0efd67e60655e1a4122a29
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-06-19 09:48:18 -0300

    Correção cards e single no branch master.

--- public/wp-content/themes/rhs/partes-templates/posts.php
+++ public/wp-content/themes/rhs/partes-templates/posts.php
@@ -1,39 +1,38 @@
 <div class="panel panel-default display-panel">
     <div class="panel-heading">
-        <?php if ( has_post_thumbnail() ) : ?>
-            <a href="<?php the_permalink(); ?>">
-                <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="" class="thumbnail_post">
-            </a>
-        <?php endif; ?>
+        <?php $userOBJ = new RHSUser( get_the_author_meta( 'ID' ) ); ?>
+        <div class="post-titulo espacamento-topo">
+            <div class="img-usuario">
+                <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"
+                   title="Ver o perfil do(a) <?php the_author_meta( 'display_name' ); ?>.">
+                    <?php echo get_avatar(get_the_author_meta( 'ID' ) ); ?>
+                </a>
+            </div>
+            <div class="box-title">
+                <span class="nome-author">
+                    <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
+                </span>
+                <span class="post_city"><?php the_ufmun(); ?></span>
+                <span class="post-date text-uppercase"><?php the_time( 'D, d/m/Y - H:i' ); ?></span>
+            </div>
+            <div class="votebox">
+                <?php do_action( 'rhs_votebox', get_the_ID() ); ?>
+            </div>
+            <div class="clearfix"></div>
+        </div>
     </div>
     <div class="panel-body">
         <a href="<?php the_permalink(); ?>"><?php the_title( '<h3 class="panel-title">', '</h3>' ); ?></a>
-        <?php the_excerpt(); ?>
+        <?php if ( has_post_thumbnail() ) : ?>
+            <a href="<?php the_permalink(); ?>">
+                <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="">
+            </a>
+            <?php 
+        endif;
+        the_excerpt(); 
+        ?>
     </div><!-- .paine-body -->
     <div class="panel-footer">
-        <?php $userOBJ = new RHSUser( get_the_author_meta( 'ID' ) ); ?>
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="post-titulo espacamento-topo">
-                    <div class="img-usuario">
-                        <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"
-                           title="Ver o perfil do(a) <?php the_author_meta( 'display_name' ); ?>.">
-                            <?php echo get_avatar(get_the_author_meta( 'ID' ) ); ?>
-                        </a>
-                    </div>
-                    <div class="box-title">
-                        <span class="nome-author">
-                            <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
-                        </span>
-                        <span class="post-date text-uppercase"><?php the_time( 'd/m/Y' ); ?></span>
-                    </div>
-                    <div class="votebox">
-                        <?php do_action( 'rhs_votebox', get_the_ID() ); ?>
-                    </div>
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-        </div>
         <div class="row">
             <div class="col-xs-6">
                 <?php
