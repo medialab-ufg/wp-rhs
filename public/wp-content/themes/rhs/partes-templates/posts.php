6d5e0a278d45986c8d44954c2fc5e6cac42e7e7a
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-06-19 08:28:32 -0300

    Correção dos cards [Master Branch]

--- public/wp-content/themes/rhs/partes-templates/posts.php
+++ public/wp-content/themes/rhs/partes-templates/posts.php
@@ -1,48 +1,52 @@
 <div class="panel panel-default display-panel">
     <div class="panel-heading">
-        <?php $userOBJ = new RHSUser( get_the_author_meta( 'ID' ) ); ?>
-        <div class="post-titulo espacamento-topo">
-            <div class="img-usuario">
-                <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"
-                   title="Ver o perfil do(a) <?php the_author_meta( 'display_name' ); ?>.">
-                    <?php echo get_avatar(get_the_author_meta( 'ID' ) ); ?>
-                </a>
-            </div>
-            <div class="box-title">
-                <span class="nome-author">
-                    <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
-                </span>
-                <span class="post_city"><?php the_ufmun(); ?></span>
-                <span class="post-date text-uppercase"><?php the_time( 'D, d/m/Y - H:i' ); ?></span>
-            </div>
-            <div class="votebox">
-                <?php do_action( 'rhs_votebox', get_the_ID() ); ?>
-            </div>
-            <div class="clearfix"></div>
-        </div>
+        <?php if ( has_post_thumbnail() ) : ?>
+            <a href="<?php the_permalink(); ?>">
+                <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="" class="thumbnail_post">
+            </a>
+        <?php endif; ?>
     </div>
     <div class="panel-body">
         <a href="<?php the_permalink(); ?>"><?php the_title( '<h3 class="panel-title">', '</h3>' ); ?></a>
-        <?php if ( has_post_thumbnail() ) : ?>
-            <a href="<?php the_permalink(); ?>">
-                <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="">
-            </a>
-            <?php
-        endif;
-        the_excerpt();
-        ?>
+        <?php the_excerpt(); ?>
     </div><!-- .paine-body -->
     <div class="panel-footer">
+        <?php $userOBJ = new RHSUser( get_the_author_meta( 'ID' ) ); ?>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="post-titulo espacamento-topo">
+                    <div class="img-usuario">
+                        <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"
+                           title="Ver o perfil do(a) <?php the_author_meta( 'display_name' ); ?>.">
+                            <?php echo get_avatar(get_the_author_meta( 'ID' ) ); ?>
+                        </a>
+                    </div>
+                    <div class="box-title">
+                        <span class="nome-author">
+                            <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
+                        </span>
+                        <span class="post-date text-uppercase"><?php the_time( 'd/m/Y' ); ?></span>
+                    </div>
+                    <div class="votebox">
+                        <?php do_action( 'rhs_votebox', get_the_ID() ); ?>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-xs-6">
                 <?php
                 if ( comments_open() ) :
                     comments_popup_link( '0 COMENTÁRIOS',
                         '<i class="fa fa-commenting-o" aria-hidden="true"></i> 1 COMENTÁRIO',
-                        '<i class="fa fa-commenting-o" aria-hidden="true"></i> % COMENTÁRIOS', 'comments-link',
+                        '<i class="fa fa-commenting-o" aria-hidden="true"></i> % COMENTÁRIOS', 'footer-link',
                         'Não é permitido Comentários neste post' );
                 endif;
                 ?>
+            </div>
+            <div class="col-xs-6">
+                <a class="text-right footer-link" href="#"><i class="fa fa-share-alt" aria-hidden="true"></i> COMPARTILHAR</a>
             </div>
         </div>
     </div>
