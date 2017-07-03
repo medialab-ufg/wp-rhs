aa0172e0209779a24175151304d5c6890003ac37
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-07-03 09:36:51 -0300

    Pagina de visualização Comunidade. (ref. #67)

---
+++ public/wp-content/themes/rhs/comunidade.php
@@ -0,0 +1,63 @@
+<?php 
+/**
+* Template name: Comunidade
+*/
+?>
+<?php get_header(); ?>
+    <div class="row comunidade">
+        <div class="col-xs-12">
+            <div class="wrapper wrapper-content animated fadeInRight">
+
+                <div class="ibox-content m-b-sm border-bottom">
+                    <div class="row">
+                    <div class="col-xs-6">
+                        <div class="pull-left m-r-md">
+                            <i class="fa fa-globe text-navy mid-icon"></i>
+                        </div>
+                        <h2>Nome da Comunidade</h2>
+                        <span>[Sobre a Comunidade]</span>
+                    </div>
+                    <div class="col-xs-4">
+                        <div class="col-xs-4">
+                            <p style="text-align: center;">0</p>
+                            <span>Mensagens</span>
+                        </div>
+                        <div class="col-xs-4">
+                            <p style="text-align: center;">0</p>
+                            <span>Comentários</span>
+                        </div>
+                        <div class="col-xs-4">
+                            <p style="text-align: center;">0</p>
+                            <span>Participantes</span>
+                        </div>
+                    </div>
+                    <div class="col-xs-2">
+                        <span class="pull-right">
+                            <button class="text-uppercase btn btn-default">seguir</button>
+                        </span>
+                    </div>           
+                    </div>         
+                </div>
+
+                <div class="ibox-content forum-container">
+
+                    <div class="forum-title">
+                        <div class="pull-right forum-desc">
+                            <small>Total posts: 0</small>
+                        </div>
+                        <h3>Posts</h3>
+                    </div>
+
+                    <div class="forum-item active">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <span>teste</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer();
