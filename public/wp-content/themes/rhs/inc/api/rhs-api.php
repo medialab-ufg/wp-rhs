08f000aa2a5383748c6334cc75b2d3552dee5808
Leo Germani <leogermani@hacklab.com.br>
2017-07-07 15:13:03 -0300

    endpoint de testes para a API refs #74

--- public/wp-content/themes/rhs/inc/api/rhs-api.php
+++ public/wp-content/themes/rhs/inc/api/rhs-api.php
@@ -2,15 +2,59 @@
 
 Class RHSApi  {
 
+    var $apinamespace = 'rhs/v1';
 
     function __construct() {
 
         add_action( 'generate_rewrite_rules', array( &$this, 'rewrite_rules' ), 10, 1 );
         add_filter( 'query_vars', array( &$this, 'rewrite_rules_query_vars' ) );
         add_filter( 'template_include', array( &$this, 'rewrite_rule_template_include' ) );
-
+        
+        add_action( 'rest_api_init', array( &$this, 'register_rest_route' ) );
+     
     }
-
+    
+    function register_rest_route() {
+        register_rest_route( $this->apinamespace, '/teste/(?P<id>\d+)', array(
+            'methods' => 'GET',
+            'callback' => array( &$this, 'get_teste' ),
+            'args' => array(
+                'id' => array(
+                    'validate_callback' => function($param, $request, $key) {
+                        return is_numeric( $param );
+                        }
+                )
+            ),
+            'permission_callback' => function ( $request ) {
+                  return current_user_can( 'edit_others_posts' );
+                }
+        ));
+    }
+    
+    
+    
+    
+    
+    ////// Endpoints
+    
+    function get_teste(WP_REST_Request $request) {
+        
+        $user = wp_get_current_user();
+        $name = $user->display_name;
+        
+        return array(
+            'current_user' => $name, 
+            'notification' => 'Você é demais!'
+        );
+    }
+    
+    
+    
+    
+    
+    
+    ////// Callback de login
+    
     function rewrite_rules( &$wp_rewrite ) {
 
         $new_rules = array(
