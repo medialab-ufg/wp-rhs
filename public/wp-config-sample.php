b050e17deda197f3de78033fb284b9a6e00c9f95
William Avelar <avelardesigner@gmail.com>
2017-07-10 09:44:25 -0300

    Include da funcao da imagem #72

--- public/wp-config-sample.php
+++ public/wp-config-sample.php
@@ -111,4 +111,5 @@ if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
+require_once(ABSPATH . '../wp-content/themes/rhs/inc/load-image.php');
 require_once(ABSPATH . 'wp-settings.php');
