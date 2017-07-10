06e9491fd11c86293ce24f6d97ab05b653473535
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-07-10 14:14:39 -0300

    Remove bug no sidebar para o formulario.

--- public/wp-content/themes/rhs/comunidade.php
+++ public/wp-content/themes/rhs/comunidade.php
@@ -42,20 +42,26 @@
         </div>
         <div class="btn-pref btn-group btn-group-justified btn-group-lg" role="group" aria-label="...">
             <div class="btn-group" role="group">
-                <button type="button" id="stars" class="btn btn-primary" href="#tab1" data-toggle="tab">
-                    <span class="glyphicon glyphicon-th" aria-hidden="true"></span>
+                <button type="button" id="sobre" class="btn btn-primary" href="#tab1" data-toggle="tab">
+                    <span class="fa fa-th" aria-hidden="true"></span>
+                    <div class="hidden-xs">Sobre</div>
+                </button>
+            </div>
+            <div class="btn-group" role="group">
+                <button type="button" id="posts" class="btn btn-default" href="#tab2" data-toggle="tab">
+                    <span class="fa fa-list-alt" aria-hidden="true"></span>
                     <div class="hidden-xs">Posts</div>
                 </button>
             </div>
             <div class="btn-group" role="group">
-                <button type="button" id="favorites" class="btn btn-default" href="#tab2" data-toggle="tab">
-                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                <button type="button" id="membros" class="btn btn-default" href="#tab3" data-toggle="tab">
+                    <span class="fa fa-user" aria-hidden="true"></span>
                     <div class="hidden-xs">Membros</div>
                 </button>
             </div>
             <div class="btn-group" role="group">
                 <a class="btn btn-default" href="#">
-                    <span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+                    <span class="fa fa-heart" aria-hidden="true"></span>
                     <div class="hidden-xs">Seguir</div>
                 </a>
             </div>
@@ -64,25 +70,26 @@
         <div class="well">
             <div class="tab-content">
                 <div class="tab-pane fade in active" id="tab1">
-                    <?php get_template_part( 'partes-templates/loop-posts'); ?>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="wrapper-content">
+                                <div class="panel">
+                                    <div class="panel-body">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="tab-pane fade in" id="tab2">
+                    <?php get_template_part( 'partes-templates/loop-posts'); ?>
+                </div>
+                <div class="tab-pane fade in" id="tab3">
                     <?php get_template_part('membro'); ?>
                 </div>
             </div>
         </div>
     </div>
-
-    <script>
-    jQuery(function($){
-        $(document).ready(function() {
-            $(".btn-pref .btn").click(function () {
-                $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-                // $(".tab").addClass("active"); // instead of this do the below 
-                $(this).removeClass("btn-default").addClass("btn-primary");   
-            });
-        });
-    });
-    </script>
 
 <?php get_footer('full');
--- public/wp-content/themes/rhs/searchform.php
+++ public/wp-content/themes/rhs/searchform.php
@@ -7,15 +7,19 @@
  * @since Rede Humaniza SuS
  */
 ?>
-<form class="form-search-rhs navbar-form navbar-left" role="search" action="<?php echo home_url('/'); ?>" method="get" id="menuPesquisa">
-    <div class="form-group" style="display: inline;">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Digite aqui o que você procura." size="15" maxlength="128" name="s" id="search" value="<?php the_search_query(); ?>">
-            <span class="input-group-btn">
-                <button type="submit" class="btn btn-default">
-                    <span class="glyphicon glyphicon-search"></span>
-                </button>
-            </span>
-        </div>
-    </div>
-</form>
+<ul>
+    <li>
+        <form class="form-search-rhs navbar-form navbar-left" role="search" action="<?php echo home_url('/'); ?>" method="get" id="menuPesquisa">
+            <div class="form-group" style="display: inline;">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Digite aqui o que você procura." size="15" maxlength="128" name="s" id="search" value="<?php the_search_query(); ?>">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn btn-default">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                    </span>
+                </div>
+            </div>
+        </form>
+    </li>
+</ul>
