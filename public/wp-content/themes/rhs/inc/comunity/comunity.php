4cfe5dab6a39ce424629494655d48c1193fdee3a
William Avelar <avelardesigner@gmail.com>
2017-07-10 09:46:22 -0300

    Include das funcoes de imagem #72

---
+++ public/wp-content/themes/rhs/inc/comunity/comunity.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+*
+* Esta Class implementa as funções necessárias para o Login e uso das reCaptcha.
+* Pega a key setada no Painel do Admin (Wordpress).
+* Com a Função display_recuperar_captcha() mostra na tela o reCaptcha.
+*
+*/
+class RHSLogin extends RHSMenssage {
+
+    private static $instance;
+
+    function __construct() {
+
+        if ( empty ( self::$instance ) ) {
+            add_filter( "login_url", array( &$this, "login_url" ), 10, 3 );
+            add_filter( "login_redirect", array( &$this, "login_redirect" ), 10, 3 );
+            add_filter( 'wp_login_errors', array( &$this, 'check_errors' ), 10, 2 );
+        }
+
+        self::$instance = true;
+    }
+
+    static function login_url( $login_url, $redirect, $force_reauth ) {
+        $login_page = home_url(RHSRewriteRules::LOGIN_URL);
+        $login_url  = add_query_arg( 'redirect_to', $redirect, $login_page );
+
+        return $login_url;
+    }
+
+    function login_redirect( $redirect_to, $requested_redirect_to, $user ) {
+        if ( empty( $redirect_to ) ) {
+            //TODO verificar role do usuário para enviar para a página apropriada
+            $redirect_to =  esc_url(home_url());
+        }
+
+        return $redirect_to;
+    }
+
+    function login_errors( $errors, $redirect_to ) {
+
+        $_SESSION['login_errors'] = '';
+    }
+    function check_errors( $errors, $redirect_to ) {
+
+        if ( $errors instanceof WP_Error && ! empty( $errors->errors ) ) {
+
+            if ( $errors->errors ) {
+
+                $this->clear_messages();
+
+                foreach ($errors->get_error_messages() as $error){
+                    $this->set_messages($error, false, 'error');
+                }
+            }
+
+            wp_redirect( home_url(RHSRewriteRules::LOGIN_URL) );
+            exit;
+        }
+
+        return $errors;
+    }
+}
+
+global $RHSLogin;
+$RHSLogin = new RHSLogin();
---
+++ public/wp-content/themes/rhs/includes/footer-full.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MediaLab01
+ * Date: 10/07/2017
+ * Time: 09:27
+ */
