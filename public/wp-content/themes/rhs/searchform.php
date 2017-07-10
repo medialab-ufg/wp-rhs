b540b0edc4a22e24e62ca77536545443f0657315
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-07-10 13:52:48 -0300

    Formulario de pesquisa retornando a busca. (ref. #75)

--- public/wp-content/themes/rhs/searchform.php
+++ public/wp-content/themes/rhs/searchform.php
@@ -7,7 +7,7 @@
  * @since Rede Humaniza SuS
  */
 ?>
-<form class="form-search-rhs navbar-form navbar-left" role="search" action="<?php echo site_url('/'); ?>" method="get" id="menuPesquisa">
+<form class="form-search-rhs navbar-form navbar-left" role="search" action="<?php echo home_url('/'); ?>" method="get" id="menuPesquisa">
     <div class="form-group" style="display: inline;">
         <div class="input-group">
             <input type="text" class="form-control" placeholder="Digite aqui o que você procura." size="15" maxlength="128" name="s" id="search" value="<?php the_search_query(); ?>">
