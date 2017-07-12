746ec7819d34e481c8bb97ababcb904a801ecc62
William Avelar <avelardesigner@gmail.com>
2017-07-12 13:44:31 -0300

    teste de emaail

--- public/wp-content/themes/rhs/functions.php
+++ public/wp-content/themes/rhs/functions.php
@@ -451,3 +451,11 @@ function facebook_meta() {
 }
 
 add_action('wp_head', 'facebook_meta', 5);
+
+function so174837_registration_email_alert( $user_id ) {
+    $user    = get_userdata( $user_id );
+    $email   = $user->user_email;
+    $message = $email . ' has registered to your website.';
+    wp_mail( 'avelardesigner@gmail.com', 'New User registration', $message );
+}
+add_action('user_register', 'so174837_registration_email_alert');
