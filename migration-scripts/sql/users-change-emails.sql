863e132e77f77a6e1ab62101620b032ce050fa07
Leo Germani <leogermani@hacklab.com.br>
2017-07-14 18:00:01 -0300

    servidor nao aguentou, mudando de todos os usuarios... (#80)

--- migration-scripts/sql/users-change-emails.sql
+++ migration-scripts/sql/users-change-emails.sql
@@ -1,15 +1,17 @@
-UPDATE {{users}} SET user_email = CONCAT('rhsteste-', user_email)
+##UPDATE {{users}} SET user_email = CONCAT('rhsteste-', user_email)
+##
+##    WHERE 
+##    
+##        ID NOT IN (
+##        
+##            SELECT DISTINCT user_id FROM {{usermeta}}
+##            WHERE meta_key = '{{prefix}}capabilities' 
+##            AND meta_value IN (
+##                'a:1:{s:13:"administrator";b:1;}',
+##                'a:1:{s:6:"editor";b:1;}'
+##            )
+##        
+##        )
+##;
 
-    WHERE 
-    
-        ID NOT IN (
-        
-            SELECT DISTINCT user_id FROM {{usermeta}}
-            WHERE meta_key = '{{prefix}}capabilities' 
-            AND meta_value IN (
-                'a:1:{s:13:"administrator";b:1;}',
-                'a:1:{s:6:"editor";b:1;}'
-            )
-        
-        )
-;
+UPDATE {{users}} SET user_email = CONCAT('rhsteste-', user_email);
