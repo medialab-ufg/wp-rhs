da302d8054c7cc9c1c93c779d0befc8c911efdab
Leo Germani <leogermani@hacklab.com.br>
2017-07-14 17:26:37 -0300

    script para mudar email dos usuarios comuns (refs #80)

---
+++ migration-scripts/sql/users-change-emails.sql
@@ -0,0 +1,15 @@
+UPDATE {{users}} SET user_email = CONCAT('rhsteste-', user_email)
+
+    WHERE 
+    
+        ID NOT IN (
+        
+            SELECT DISTINCT user_id FROM {{usermeta}}
+            WHERE meta_key = '{{prefix}}capabilities' 
+            AND meta_value IN (
+                'a:1:{s:13:"administrator";b:1;}',
+                'a:1:{s:6:"editor";b:1;}'
+            )
+        
+        )
+;
