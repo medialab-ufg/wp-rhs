7ee4e225450e329421f826248150d45e83a4e5fb
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-07-18 09:07:25 -0300

    Página com listagem de todas as notificações. (ref. #81)

---
+++ public/wp-content/themes/rhs/notificacoes.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+* Template name: Notificações
+*/
+?>
+<?php RHSHtml::setTitulo('Notificações'); ?>
+<?php get_header(); ?>
+            <div class="row">
+                <div class="col-xs-12 notificacoes">
+                    <div class="wrapper-content">
+                        <div class="panel panel-default panel-horizontal">
+                            <div class="panel-heading">IMAGEM</div>
+                            <div class="panel-body">NOTIFICAÇÃO</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+<?php get_footer();
