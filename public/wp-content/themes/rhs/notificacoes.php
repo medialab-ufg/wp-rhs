dec515f793a2e4120be8a92ff5589677d2c9d441
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-07-18 10:44:05 -0300

    Página de listagem de todas as notificações. (ref. #81)

--- public/wp-content/themes/rhs/notificacoes.php
+++ public/wp-content/themes/rhs/notificacoes.php
@@ -9,8 +9,14 @@
                 <div class="col-xs-12 notificacoes">
                     <div class="wrapper-content">
                         <div class="panel panel-default panel-horizontal">
-                            <div class="panel-heading">IMAGEM</div>
-                            <div class="panel-body">NOTIFICAÇÃO</div>
+                            <div class="panel-heading">
+                                <img src="http://www.knowmuhammad.org/img/default_avatar.gif" class="img-responsive img-circle" width="50">
+                            </div>
+                            <div class="panel-body">
+                                <span>Fabiano comentou no post "Teste de notificação."</span>
+                                <small>Em 5 dias e 18 horas atrás.</small>
+                                <button class="btn btn-default text-uppercase">Marcar como Lidas</button>
+                            </div>
                         </div>
                     </div>
                 </div>
