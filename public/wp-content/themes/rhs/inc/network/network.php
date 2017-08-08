e45b7969d30011881c77148735aebc76d4224667
Leo Germani <leogermani@hacklab.com.br>
2017-08-08 14:28:05 -0300

    fix salvar estatisticas de compartilhamento e view (refs: #64)

--- public/wp-content/themes/rhs/inc/network/network.php
+++ public/wp-content/themes/rhs/inc/network/network.php
@@ -6,9 +6,11 @@ class RHSNetwork {
     const META_KEY_TWITTER = 'rhs_data_twitter';
     const META_KEY_WHATSAPP = 'rhs_data_whatsapp';
     const META_KEY_VIEW = 'rhs_data_view';
+    const META_KEY_PRINT = 'rhs_data_print';
 
     public function __construct() {
-        add_action('wp_ajax_nopriv_add_data', array( &$this, 'add_data' ) );
+        add_action('wp_ajax_nopriv_rhs_add_stats_data', array( &$this, 'add_data' ) );
+        add_action('wp_ajax_rhs_add_stats_data', array( &$this, 'add_data' ) );
         add_action('wp_body_init', array( &$this, 'js_api_network'));
         add_action('wp_body_init', array( &$this, 'js_api_network'), 10, 1);
         add_action('wp_enqueue_scripts', array( &$this,'network_js'));
@@ -17,6 +19,13 @@ class RHSNetwork {
     public function network_js(){
         if(is_single()){
             wp_enqueue_script('RHSNetworkJS', get_template_directory_uri() . '/inc/network/network.js', '','', true);
+            wp_localize_script('RHSNetworkJS', 'RHSNetworkJS', array(
+                'META_KEY_FACEBOOK' => 'rhs_data_facebook',
+                'META_KEY_TWITTER' => 'rhs_data_twitter',
+                'META_KEY_WHATSAPP' => 'rhs_data_whatsapp',
+                'META_KEY_VIEW' => 'rhs_data_view',
+                'META_KEY_PRINT' => 'rhs_data_print'
+            ));
         }
     }
 
