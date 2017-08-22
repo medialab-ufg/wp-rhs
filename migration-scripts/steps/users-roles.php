020acdbce6d82ae8a2aef72a8c73b66b010d900d
Pedro Naves <cambaleado@gmail.com>
2017-08-22 15:32:04 -0300

    alterando scripts de importação para utilização de nova função (ref. #115)

--- migration-scripts/steps/users-roles.php
+++ migration-scripts/steps/users-roles.php
@@ -3,18 +3,14 @@
 $this->log('Limpando metadados de usuários com ID maior que 1');
 $wpdb->query("DELETE FROM $wpdb->usermeta WHERE user_id > 1 AND meta_key = 'rhs_capabilities';");
 
-$query = $this->get_sql('users-roles-1');
 $this->log('Importando administradores...');
-$wpdb->query($query);
+$this->query('users-roles-1');
 
-$query = $this->get_sql('users-roles-2');
 $this->log('Importando editores (excluindo-se os que já são administradores)...');
-$wpdb->query($query);
+$this->query('users-roles-2');
 
-$query = $this->get_sql('users-roles-3');
 $this->log('Importando votantes (excluindo-se os que já tem algum papel)...');
-$wpdb->query($query);
+$this->query('users-roles-3');
 
-$query = $this->get_sql('users-roles-4');
 $this->log('Importando o restante como autores...');
-$wpdb->query($query);
+$this->query('users-roles-4');
