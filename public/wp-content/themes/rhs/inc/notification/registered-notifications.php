c0639a718112c60847f32e1082055b49360c77d8
Leo Germani <leogermani@hacklab.com.br>
2017-08-25 09:30:06 -0300

    documentacao do registro de notificacoes

--- public/wp-content/themes/rhs/inc/notification/registered-notifications.php
+++ public/wp-content/themes/rhs/inc/notification/registered-notifications.php
@@ -1,4 +1,19 @@
 <?php
+
+/**
+ * Neste arquivo registramos as notificações
+ * 
+ * As chaves do array são os hooks, que estão espalhados pelo código do sistema. Por exemplo, quando publica um novo post, ou quando umusuário é promovido.
+ *
+ * Os valores são o tipo de notificação que será gerado. 
+ *
+ * É preciso que exista uma subclasse de RHSNotification com o nome deste tipo. Estas subclasses ficam na pasta types (ou podem ser incuídas via plugin).
+ *
+ * A subclasse do tipo de notificação deve implementar um método chamado verify(), que vai receber o parâmetro que o hook passa e criar uma notificação em um ou mais canais.
+ * 
+ * 
+ */
+
 return [
 
     'rhs_post_promoted' => 'post_promoted'
