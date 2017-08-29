4ec5b717682bd2be2ea9d3171a2c8f867a37a395
Pedro Naves <cambaleado@gmail.com>
2017-08-29 10:37:23 -0300

    retirando comentário de função para importação (ref. #122)

--- migration-scripts/steps/posts-follow.php
+++ migration-scripts/steps/posts-follow.php
@@ -9,8 +9,8 @@ $this->log('Limpando metadados de seguir usuários');
 $wpdb->query("DELETE FROM $wpdb->usermeta
 	WHERE meta_key = '" . RHSFollowPost::FOLLOW_POST_KEY . "';");
 
-// $this->log('Importando informação de seguidores de post');
-// $this->query('posts-follow', $substitutions);
+$this->log('Importando informação de seguidores de post');
+$this->query('posts-follow', $substitutions);
 
-// $this->log('Importando informação de posts seguidos');
-// $this->query('posts-followed', $substitutions);
+$this->log('Importando informação de posts seguidos');
+$this->query('posts-followed', $substitutions);
