4461ca034345464cb18209348694baeec250f818
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-09-04 15:33:34 -0300

    Widgets de facebok e twitter (ref. #135)

--- public/wp-content/themes/rhs/header-full.php
+++ public/wp-content/themes/rhs/header-full.php
@@ -20,6 +20,17 @@
     <?php wp_head(); ?>
 </head>
 <body>
+<!-- SDK Facebook -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.10&appId=1018555378249909";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- Fim SDK Facebook -->
+
 <!-- Tag header para o Primeiro Menu -->
 <header id="navBar-top">
     <nav class="navbar navbar-default">
