e7d3c643a79d8a4998e99f15993b139d838c1292
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-09-05 12:16:32 -0300

    template de categorias (ref. #104)

--- public/wp-content/themes/rhs/category.php
+++ public/wp-content/themes/rhs/category.php
@@ -1,17 +1,19 @@
 <?php get_header(); ?>
-    <div class="row categoria">
+    <div class="row">
         <div class="col-xs-12">
-            <h1 class="titulo-page">
-                <?php
-                     _e('Categoria ');
-                     echo wp_title( );
-                ?>
-            </h1>
-        </div>
-        <div class="col-xs-12 descricao text-center">
-            <?php
-                echo category_description();
-            ?>
+            <div class="panel categoria">
+                <h1 class="titulo">
+                    <?php
+                        _e('Categoria ');
+                        echo wp_title( );
+                    ?>
+                </h1>
+                <div class="descricao text-left">
+                    <?php
+                        echo category_description();
+                    ?>
+                </div>
+            </div>
         </div>
     </div>
     <?php get_template_part( 'partes-templates/loop-posts'); ?>
