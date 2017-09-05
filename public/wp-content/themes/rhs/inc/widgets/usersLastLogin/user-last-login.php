914042c77d07eb48fe2d827616e6f2a60d1ce97a
Pedro Naves <cambaleado@gmail.com>
2017-09-05 11:10:38 -0300

    widget para listar participantes (ref. #142)

---
+++ public/wp-content/themes/rhs/inc/widgets/usersLastLogin/user-last-login.php
@@ -0,0 +1,96 @@
+<?php
+function user_last_login() {
+    register_widget('User_Last_Login');
+}
+
+add_action('widgets_init', 'user_last_login');
+
+class User_Last_Login extends WP_Widget {
+
+	function __construct() {
+		parent::__construct(
+			'user_last_login', 
+			__('Lista de Participantes', 'text_domain'),
+			array( 'description' => __('Lista de Participantes', 'text_domain' ), )
+		);
+	}
+	
+	public function get_user_list($number_of_users) {
+		$args = array(
+            'meta_key'  => '_last_login',
+            'order'     => 'ASC',
+            'number'    => $number_of_users,
+        );
+        $user_query = new WP_User_Query($args);
+		return $user_query;
+	}
+    
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget($args, $instance) {
+		$number_of_users = !empty($instance['number_of_users']) ? $instance['number_of_users'] : '';
+		?> 
+		<?php if($number_of_users): ?>
+		
+		<aside class="widget widget_meta">
+			<h2 class="widget-title">Participantes</h2>
+			<?php 
+				$user_query = self::get_user_list($number_of_users);
+				if (!empty($user_query->results)) {	
+					foreach ($user_query->results as $user) {
+			?>
+				<div class="user-last-login-widget">
+					<a href="<?php echo get_author_posts_url($user->ID); ?>"><?php echo get_avatar($user->ID, '50'); ?></a>
+				</div>
+			
+			<?php 
+					}
+				}
+			?>
+		</aside>
+		<?php endif; ?>
+		<?php
+	}
+
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form($instance) {
+		$number_of_users = !empty($instance['number_of_users']) ? $instance['number_of_users'] : __('10', 'text_domain');
+		?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('number_of_users'); ?>"><?php _e('Número de participantes para exibir:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('number_of_users'); ?>" name="<?php echo $this->get_field_name('number_of_users'); ?>" type="text" value="<?php echo esc_attr($number_of_users); ?>">
+		</p>
+
+		<?php
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update($new_instance, $old_instance) {
+		$instance = array();
+		$instance['number_of_users'] = (!empty($new_instance['number_of_users'])) ? strip_tags($new_instance['number_of_users']) : '';
+		return $instance;
+	}
+}
