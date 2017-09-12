cd273a8d4355f833729efad88c1bd3579a986399
Leo Germani <leogermani@hacklab.com.br>
2017-09-11 22:10:27 -0300

    Adicionado suporte as URLS no padrão antigo para comunidades (refs #146)

--- public/wp-content/themes/rhs/inc/rewrite-rules/rewrite-rules.php
+++ public/wp-content/themes/rhs/inc/rewrite-rules/rewrite-rules.php
@@ -73,7 +73,8 @@ class RHSRewriteRules {
             'user/me/edit' . "/?$"  => "index.php?rhs_custom_login=1&rhs_login_tpl=" . self::PROFILE_URL,
             'node/add/blog' . "/?$" => "index.php?rhs_custom_login=1&rhs_login_tpl=" . self::POST_URL,  
             'tags/([^/]+)/?'        => 'index.php?tag=$matches[1]',
-            'category/tags/([^/]+)/?'        => 'index.php?tag=$matches[1]'
+            'category/tags/([^/]+)/?'        => 'index.php?tag=$matches[1]',
+            'grupos/([^/]+)/?'        => 'index.php?'.RHSComunities::TAXONOMY.'=$matches[1]'
         );
         
 
