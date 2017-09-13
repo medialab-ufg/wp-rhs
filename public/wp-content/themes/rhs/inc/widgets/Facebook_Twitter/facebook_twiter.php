a050bbfa91e5bc3d5e20f99d19fb29b99ecf1ea2
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-09-13 14:10:49 -0300

    Retornando jquery do facebook e twitter widget.

--- public/wp-content/themes/rhs/inc/widgets/Facebook_Twitter/facebook_twiter.php
+++ public/wp-content/themes/rhs/inc/widgets/Facebook_Twitter/facebook_twiter.php
@@ -6,7 +6,7 @@ class Facebook_Twitter extends WP_Widget {
 	 * Sets up the widgets name etc
 	 */
 	public function __construct() {
-		//add_action('wp_enqueue_scripts', array($this, 'js_widget'));
+		add_action('wp_enqueue_scripts', array($this, 'js_widget'));
 		$widget_ops = array( 
 			'classname' => 'Facebook_Twitter',
 			'description' => 'Widget para exibir a Pagina do Facebook e o botão Seguir do Twitter',
@@ -14,11 +14,11 @@ class Facebook_Twitter extends WP_Widget {
 		parent::__construct( 'facebook_twitter', 'Facebook_Twitter', $widget_ops );
 	}
 
-    /*public function js_widget() {
+    public function js_widget() {
 		if ( is_active_widget( false, false, $this->id_base, true ) ) {
 			wp_enqueue_script('facebook_twitter', get_template_directory_uri() . '/inc/widgets/Facebook_Twitter/facebook_twitter.js', array('jquery'));
 		}
-	}*/
+	}
 
 	/**
 	 * Outputs the content of the widget
