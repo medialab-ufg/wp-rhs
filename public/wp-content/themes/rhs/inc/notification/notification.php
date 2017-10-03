32edbac7049d2601190d86d4c36e15718644fdf3
Pedro Naves <cambaleado@gmail.com>
2017-10-03 15:40:41 -0300

    corrigindo id de post da comunidade e adicionando botão para ver usuário em post sendo seguido (ref. #198)

--- public/wp-content/themes/rhs/inc/notification/notification.php
+++ public/wp-content/themes/rhs/inc/notification/notification.php
@@ -74,7 +74,7 @@ class RHSNotification {
                 $buttons[] = (object) array('id' => 'open_' . $type, 'text' => 'Ver Comentário');
                 break;
             case 'new_community_post':
-                $buttons[] = (object) array('id' => 'open' . $type, 'text' => 'Ver Post');
+                $buttons[] = (object) array('id' => 'open_' . $type, 'text' => 'Ver Post');
                 break;
             case 'new_post_from_user':
                 $buttons[] = (object) array('id' => 'open_post_' . $type, 'text' => 'Ver Post');
@@ -82,6 +82,7 @@ class RHSNotification {
                 break;
             case 'post_followed':
                 $buttons[] = (object) array('id' => 'open_' . $type, 'text' => 'Ver Post');
+                $buttons[] = (object) array('id' => 'open_user_' . $type, 'text' => 'Ver Usuário');
                 break;
             case 'replied_ticket':
                 $buttons[] = (object) array('id' => 'open_' . $type, 'text' => 'Ver Resposta');
