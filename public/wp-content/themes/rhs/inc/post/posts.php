0e7fc289eab191a4a5bcb94ba45c7df666bbdea1
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-11-09 13:59:07 -0200

    Alterado o nome da função para um melhor intendimento.

--- public/wp-content/themes/rhs/inc/post/posts.php
+++ public/wp-content/themes/rhs/inc/post/posts.php
@@ -15,7 +15,7 @@ class RHSPosts extends RHSMessage {
         add_action( 'wp_ajax_nopriv_get_tags', array( &$this, 'ajax_get_tags' ) );
         add_filter( 'the_editor', array( &$this, 'add_placeholder_editor' ) );
         add_filter( 'mce_external_plugins', array( &$this, 'add_mce_placeholder_plugin' ) );
-        add_filter( 'save_post', array( &$this, 'add_meta_date' ) );
+        add_filter( 'save_post', array( &$this, 'add_meta_date_and_notification' ) );
         add_action( 'wp_enqueue_scripts', array( &$this, 'addJS' ));
         add_action( 'wp_ajax_apagar_post_toggle', array(&$this, 'apagar_post_toggle'));
 
@@ -661,7 +661,7 @@ class RHSPosts extends RHSMessage {
         endwhile;
     }
     
-    function add_meta_date( $postID ) {
+    function add_meta_date_and_notification( $postID ) {
 
         $data = get_post($postID);
         
