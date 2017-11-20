cb7bbe3f54c1a6dbe3f3f56ef379c822092cca59
Rodrigo <rodrigodeoliveira@hotmail.com.br>
2017-11-20 14:47:38 -0200

    Pequenos detalhes UI

--- public/wp-content/themes/rhs/partes-templates/export-modal.php
+++ public/wp-content/themes/rhs/partes-templates/export-modal.php
@@ -4,7 +4,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Exportação de Resultados</h4>
+        <h3 class="modal-title">Exportar Resultados da Busca para CSV</h3>
       </div>
       <div class="modal-body text-center">
         <?php show_results_from_search(); ?>
