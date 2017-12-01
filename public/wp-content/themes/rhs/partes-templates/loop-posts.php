6f8a402fc7b6fcf2697b22f1489da41e58a08700
Rodrigo de Oliveira <rodrigodeoliveira@hotmail.com.br>
2017-12-01 14:33:33 -0200

    Adaptação para não renderizar nem a div container do post privado

--- public/wp-content/themes/rhs/partes-templates/loop-posts.php
+++ public/wp-content/themes/rhs/partes-templates/loop-posts.php
@@ -1,21 +1,20 @@
 <?php if(have_posts()) : ?>
 
 	<div class="clearfix masonry">
-		<div class="grid-sizer"></div>
-		<div class="gutter-sizer"></div>
-		<?php while (have_posts()): 
-			the_post();
-		?>
-			<div class="grid-item">
-				<?php
-                    $is_the_author = ( is_user_logged_in() && is_author(get_current_user_id()) );
-                    if( "private" != get_post_status() ||  $is_the_author) {
-                        //Pega o painel dos posts para mostrar na pagina front-page os posts.
-                        get_template_part( 'partes-templates/posts');
-                    }
-				?>
-			</div>
-		<?php endwhile; ?>
+		<div class="grid-sizer"></div> <div class="gutter-sizer"></div>
+        <?php
+        while (have_posts()): the_post();
+            $is_the_author = ( is_user_logged_in() && is_author(get_current_user_id()) );
+
+            // Pega o painel dos posts para mostrar na pagina front-page os posts,
+            // exibindo os posts privados apenas para o autor dos mesmos
+            if( "private" != get_post_status() || $is_the_author ): ?>
+
+                <div class="grid-item"> <?php get_template_part( 'partes-templates/posts'); ?> </div>
+
+                <?php
+            endif;
+		endwhile; ?>
 	</div>
 
 	<div class="col-xs-12">
@@ -23,5 +22,7 @@
 			<?php paginacao_personalizada(); ?>
 		</div>
 	</div>
+
 <?php else : get_template_part('partes-templates/none'); ?>
+
 <?php endif; ?>
