87026c914da57be30dcf2232aba5a9c7c9c0b6c5
Rodrigo <rodrigodeoliveira@hotmail.com.br>
2017-12-01 16:36:43 -0200

    Inverte a lógica do algoritmo para otimizá-lo

--- public/wp-content/themes/rhs/partes-templates/loop-posts.php
+++ public/wp-content/themes/rhs/partes-templates/loop-posts.php
@@ -6,26 +6,20 @@
 
         $_is_community = $wp_query->is_tax(RHSComunities::TAXONOMY);
         while( have_posts() ):
-            $_show_post = false;
+            $_show_post = true;
             the_post();
 
             /*
-             * Se estamos na lista de posts da comunidade, exibimos também os posts privados.
+             * Se estamos na lista de posts da comunidade, exibimos também os posts privados (daquela comunidade).
              * Caso contrário, só listamos posts privados se o autor corrente for o autor do post
              */
-            if($_is_community) {
-                $_show_post = true;
-            } else {
-                if("private" === get_post_status()) {
-                    $_is_the_author = get_current_user_id() === get_the_author_meta('ID');
-                    if($_is_the_author) {
-                        $_show_post = true;
-                    }
-                } else {
-                    $_show_post = true;
+            if( !$_is_community )  {
+                $_is_the_author = get_current_user_id() === get_the_author_meta('ID');
+                if( "private" === get_post_status() && ! $_is_the_author ) {
+                    $_show_post = false;
                 }
             }
-
+            
             if($_show_post): ?>
                 <div class="grid-item"> <?php get_template_part( 'partes-templates/posts'); ?> </div>
                 <?php
