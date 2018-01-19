cb52df7f0d5e616ccedc6717aec6ccca0033954b
Pedro Naves <oi@pedronaves.com.br>
2018-01-19 11:09:31 -0200

    corrigindo paginação personalizada (ref. #195)

--- public/wp-content/themes/rhs/functions.php
+++ public/wp-content/themes/rhs/functions.php
@@ -362,8 +362,10 @@ function menuRodape(){
 function paginacao_personalizada() {
     global $wp_query;
     $big = 999999999;
+    $search_for   = array($big, '#038;');
+    $replace_with = array('%#%', '');       
     $pages = paginate_links(array(
-        'base' => str_replace($big, '%#%', get_pagenum_link($big)),
+        'base' => str_replace($search_for, $replace_with, esc_url(get_pagenum_link($big))),
         'format' => '?page=%#%',
         'current' => max(1, get_query_var('paged')),
         'total' => $wp_query->max_num_pages,
