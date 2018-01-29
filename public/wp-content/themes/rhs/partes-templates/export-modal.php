02a1a1d2aed0590f04aa28480546416d3a311728
Pedro Naves <oi@pedronaves.com.br>
2018-01-29 14:13:49 -0200

    adicionando exportação para xls (ref. #195)

--- public/wp-content/themes/rhs/partes-templates/export-modal.php
+++ public/wp-content/themes/rhs/partes-templates/export-modal.php
@@ -4,10 +4,15 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h3 class="modal-title">Exportar Resultados da Busca para CSV</h3>
+        <h3 class="modal-title">Exportar Resultados da Busca para <span id="format_file"></span></h3>
       </div>
       <div class="modal-body text-center">
-        <?php show_results_from_search(); ?>
+        <div id='result_type_csv'>
+          <?php show_results_from_search('csv'); ?>
+        </div>
+        <div id='result_type_xls'>
+          <?php show_results_from_search('xls'); ?>
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Sair</button>
