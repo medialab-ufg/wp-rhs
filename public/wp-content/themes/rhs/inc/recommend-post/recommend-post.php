b80ec2650c6200edde09c236241390cbea6a6f2d
Pedro Naves <oi@pedronaves.com.br>
2018-02-09 10:26:31 -0200

    mend

--- public/wp-content/themes/rhs/inc/recommend-post/recommend-post.php
+++ public/wp-content/themes/rhs/inc/recommend-post/recommend-post.php
@@ -1,6 +1,8 @@
 <?php
 
 class RHSRecommendPost extends RHSMessage {
+    const RECOMMEND_POST_TO_KEY = '_rhs_recommend_post_to';
+    const RECOMMEND_POST_FROM_KEY = '_rhs_recommend_post_from';
 
     function __construct() {
         add_action('wp_enqueue_scripts', array(&$this, 'addJS'));
@@ -47,12 +49,19 @@ class RHSRecommendPost extends RHSMessage {
 
         $this->clear_messages();
 
+        $current_user = wp_get_current_user();
         $user = new RHSUser(get_userdata($_POST['user_id']));
-
+        $user_id = $_POST['user_id'];
+        $post_id = $_POST['post_id'];
+        
         $data['user'] = array(
-            'user_id' => $_POST['user_id'],
-            'post_id' => $_POST['post_id'],
+            'user_id' => $user_id,
+            'post_id' => $post_id,
+            'recommend_from' => $current_user->ID
         );
+
+        add_user_meta($user_id, self::RECOMMEND_POST_TO_KEY, $data['user']);
+        add_user_meta($current_user->ID, self::RECOMMEND_POST_FROM_KEY, $data['user']);
 
         $this->set_messages($user->get_name() . ' recebeu a indicação de leitura', false, 'success');
 
