b61fd73d7b52e9bbdfb14e8d041bab73989f3567
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-06-09 00:41:42 -0300

    Melhora consideravelmente layout da busca de usuarios

--- public/wp-content/themes/rhs/partes-templates/header_search_users.php
+++ public/wp-content/themes/rhs/partes-templates/header_search_users.php
@@ -1,17 +1,13 @@
-<div class="container">
+<div class="container col-md-12 no-padding">
     <form action="<?php echo home_url('/'); ?>busca/usuarios/" id="filter">
-        <div class="col-xs-12 col-sm-7">
-            <div class="form-inline col-xs-12">    
-                <?php RHSSearch::render_uf_city_select(); ?>
-            </div>
+
+        <div class="col-md-6 no-padding">
+            <?php RHSSearch::render_uf_city_select(); ?>
         </div>
-        <div class="col-xs-12 col-sm-5">
-            <div class="form-inline col-xs-12">
-                <div class="form-group">
-                    <label for="keyword" class="control-label">Nome ou E-mail</label>
-                    <input type="text" name="keyword" id="keyword" class="form-control" value="<?php echo RHSSearch::get_param('keyword'); ?>">
-                </div>
-            </div>
+
+        <div class="col-md-6 form-group ">
+            <label for="keyword" class="control-label">Nome ou E-mail</label>
+            <input type="text" name="keyword" id="keyword" class="form-control" value="<?php echo RHSSearch::get_param('keyword'); ?>">
         </div>
 
         <?php echo RHSSearch::getSearchButtons(); ?>
