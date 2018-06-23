74a394e7a4f1dfeb53aace7ecd0d7543b202df4c
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-06-23 18:29:07 -0300

    placeholder p campo de usuarios

--- public/wp-content/themes/rhs/partes-templates/header_search_users.php
+++ public/wp-content/themes/rhs/partes-templates/header_search_users.php
@@ -7,7 +7,7 @@
 
         <div class="col-md-6 form-group">
             <label for="keyword" class="control-label">Nome ou E-mail</label>
-            <input type="text" name="keyword" id="keyword" class="form-control" value="<?php echo RHSSearch::get_param('keyword'); ?>">
+            <input type="text" placeholder="Filtre usuários da RHS por nome ou e-mail" name="keyword" id="keyword" class="form-control" value="<?php echo RHSSearch::get_param('keyword'); ?>">
         </div>
 
         <?php echo RHSSearch::getSearchButtons(); ?>
