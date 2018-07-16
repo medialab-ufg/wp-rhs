0c003cd14d8bc32200739e7a0d75218eb3c90af7
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-07-16 15:07:47 -0300

    Apenas próprio usuário pode ver página com os posts que ele segue (#265)

--- public/wp-content/themes/rhs/posts_seguidos.php
+++ public/wp-content/themes/rhs/posts_seguidos.php
@@ -1,42 +1,44 @@
-<?php $title_box = 'Posts seguidos'; ?>
-<?php get_header('full'); ?>
-<?php $meta = RHSFollow::FOLLOWED_POSTS_KEY; ?>
 <?php
-$current_user = wp_get_current_user();
-
+$_current_user = get_current_user_id();
 $author_data = get_queried_object();
-if ($author_data->has_prop('ID')) {
+
+if (!is_null($author_data) && $author_data->has_prop('ID'))
     $author_id = $author_data->get('ID');
-}
-$current_user->ID == $author_id ? $var = true : $var = false;
 
-$paged = !empty(get_query_var('rhs_paged')) ? get_query_var('rhs_paged') : 1;
-?>
+if (isset($author_id)) {
+    if ($author_id != $_current_user):
+        wp_redirect(home_url());
+    else:
+         get_header('full');
+         $meta = RHSFollow::FOLLOWED_POSTS_KEY;
+         $paged = !empty(get_query_var('rhs_paged')) ? get_query_var('rhs_paged') : 1;
+        ?>
 
-    <div class="row">
-        <div class="col-xs-9">
-            <?php include(locate_template('partes-templates/user-header-info.php')); ?>
+        <div class="row">
+            <div class="col-xs-9">
+                <?php include(locate_template('partes-templates/user-header-info.php')); ?>
 
-            <h1 class="titulo-page">
-                <?php ($var) ? _e('Posts que eu sigo') : printf('Quem %s segue', $author_data->get('display_name')); ?>
-            </h1>
-            <div class="tab-content">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <?php include(locate_template('partes-templates/followed-posts.php')); ?>
+                <h1 class="titulo-page"> Posts que eu sigo </h1>
+                <div class="tab-content">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <?php include(locate_template('partes-templates/followed-posts.php')); ?>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="col-xs-3">
+                <?php get_sidebar(); ?>
+            </div>
         </div>
 
-        <div class="col-xs-3">
-            <?php get_sidebar(); ?>
-        </div>
-    </div>
-
-<?php get_footer('full');
-
+        <?php get_footer('full');
+    endif;
+} else {
+    wp_redirect(home_url());
+}
