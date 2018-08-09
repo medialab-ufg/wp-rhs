dbc9039088088522b7514ebf598c2db9f87a0829
andre2ar <andre2ar@outlook.com>
2018-08-09 10:27:09 -0300

    Importação de anexos #182

--- migration-scripts/steps/attachments.php
+++ migration-scripts/steps/attachments.php
@@ -1,10 +1,34 @@
 <?php
 $this->log('Importando anexos dos posts...');
 $results = $this->get_results('attachments');
-$server_base_url = "http://lab.redehumanizasus.net/";
+
+$attachments_path_dir = "";//Diretorio de anexos deve ser configurado
+
 foreach ($results as $attachment)
 {
-    $url = $server_base_url . $attachment['filepath'];
-    $this->insert_attachment_from_url($url, $attachment['id']);
+    if(strpos($attachment['filepath'], '/files') !== false)
+    {
+        $file_path = $attachments_path_dir . end(explode("files/", $attachment['filepath']));
+        if(file_exists($file_path))
+        {
+            $filetype = end(explode(".", $file_path));
+            $filename = $attachment['filename'];
+            $attach = array(
+                'guid'           => $file_path,
+                'post_mime_type' => $filetype,
+                'post_title'     => $filename,
+                'post_content'   => '',
+                'post_status'    => 'inherit'
+            );
+
+            $attach_id = wp_insert_attachment( $attach, $filename, $attachment['id'] );
+
+            require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+            $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+            wp_update_attachment_metadata( $attach_id, $attach_data );
+        }
+    }
 }
+
 $this->log('Anexos importados');
