6415b9c4dbb47ceaf720ac4eb95823bc7a1f05ca
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-09-03 21:13:45 -0300

    Altera msg de compilacao concluida

--- compile-sass.sh
+++ compile-sass.sh
@@ -2,7 +2,7 @@
  
 # Executa o comando 'sass' para verificar se existe (veja http://stackoverflow.com/a/677212/329911)
 command -v sass >/dev/null 2>&1 || {
-  echo >&2 "SASS parece não está disponivel.";
+  echo >&2 "SASS parece não estar disponivel. Abortando ...";
   exit 1;
 }
 
@@ -11,7 +11,6 @@ cd public/wp-content/themes/rhs/assets/scss/
 echo "Compilando Sass..." 
 sass style.scss:../../style.css
 # sass -E 'UTF-8' rhs_editor_style.scss:../../rhs_editor_style.css / arquivo nao existe
-echo "Sass Compilado"
- 
-echo "Compilação do Sass Concluído!"
+echo "Sass compilado com sucesso. \nAtualize o navegador para ver as mudanças."
+
 exit 0
