344ccc845d89ea27bf665ded584ba8451b35a0c5
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-09-08 17:57:26 -0300

    lista comentários do usuário (#264)

--- public/wp-content/themes/rhs/comentarios.php
+++ public/wp-content/themes/rhs/comentarios.php
@@ -9,18 +9,46 @@ $c = get_comments(['user_id' => get_current_user_id()]);
 
         <h1 class="titulo-page"> Meus comentários </h1>
         <div class="tab-content">
-            <div class="row">
-                <div class="col-xs-12">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <?php foreach($c as $comentario) {
-                                if ($comentario instanceof WP_Comment) {
-                                    echo $comentario->comment_content;
-                                    echo " <br> - " . get_permalink($comentario->comment_post_ID) . "<hr>";
-                                }
-                            } ?>
-                        </div>
-                    </div>
+
+            <div class="col-xs-12 panel panel-default">
+                <div class="panel-body">
+                    <ul class="list-group" id="followContent">
+                        <?php
+
+                        if (count($c) > 0) {
+                            foreach($c as $comentario) {
+                                if ($comentario instanceof WP_Comment) { ?>
+
+                                    <li class="list-group-item">
+
+                                        <div class="col-xs-10 text-left">
+                                            <p>
+                                                <span class="span">Em: </span>
+                                                <a href="<?php echo get_permalink($comentario->comment_post_ID); ?>">
+                                                    <?php echo get_post($comentario->comment_post_ID)->post_title; ?>
+                                                </a>
+                                            </p>
+                                            <p>
+                                                <?php echo $comentario->comment_content; ?>
+                                            </p>
+
+                                        </div>
+                                        <div class="col-md-2">
+                                            <strong style="color: #003c46">Status</strong>
+                                            <br>
+                                            <?php echo wp_get_comment_status($comentario->comment_ID); ?>
+                                        </div>
+                                        <div class="clearfix"></div>
+                                    </li>
+
+                                <?php }
+
+                            }
+                        } else {
+                         echo '<p class="text-center">Você ainda não comentou em nenhuma postagem.</p>';
+                        }
+                        ?>
+                    </ul>
                 </div>
             </div>
         </div>
